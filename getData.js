775db4aaadbca3e800a67a831e2ac1371c83d1eb
Egor Kosyakov <ra4nd0m@gmail.com>
2023-10-18 15:56:57 +0300

    Added data fetcher

---
+++ getData.js
@@ -0,0 +1,80 @@
+const materialGroups = {
+    ferro1: [],
+    ferro2: [],
+    steel: [],
+    raw: []
+};
+
+const materialIds = {
+    ferro1: [72,
+        73,
+        18,
+        74,
+        75,
+        76,
+        77,
+        19,
+        17,
+        22],
+    ferro2: [78,
+        79,
+        80,
+        81,
+        20,
+        82,
+        21,
+        83,
+        23],
+    steel: [],
+    raw: [2,
+        6,
+        8,
+        4,
+        1,
+        69,
+        5,
+        66]
+};
+async function makeMaterialRequests() {
+    for (const group in materialGroups) {
+        const ids = materialIds[group];
+        for (const id of ids) {
+            try {
+                const requestBody = {
+                    "date": "2023-03-15",
+                    "material_id": id,
+                    "property_id": 1
+                };
+                const data = await makeRequest(requestBody);
+                materialGroups[group].push(data);
+            } catch (err) {
+                console.error(err);
+                continue;
+            }
+        }
+    }
+    console.log(materialGroups);
+    return materialGroups;
+}
+
+// Function to make the request
+async function makeRequest(requestBody) {
+    const response = await fetch('http://base.metallplace.ru:8080/getChangeSummary', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVrX3Rlc3RpbmciLCJleHAiOjkyMjMzNzIwMzY4NTQ3NzU4MDd9.klvYnKco5Y2AdSmXDcNhLYYUBCjsMO_M2Ubmdm1Uv3M"
+        },
+        body: JSON.stringify(requestBody)
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.value;
+}
+
+module.exports={makeMaterialRequests}
+makeMaterialRequests();
