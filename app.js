65ee78008077d44ade04f588fdafb454f34b6971
Egor Kosyakov <ra4nd0m@gmail.com>
2023-10-18 14:18:29 +0300

    things got done

--- app.js
+++ app.js
@@ -14,17 +14,22 @@ function addValue(main, mainValue, sub, subValue, element) {
     childElementSub.textContent = subValue + '%';
     valueElement.appendChild(childElementMain);
     valueElement.appendChild(childElementSub);
-    element.appendChild(valueElement);
-    return element;
+    return valueElement;
 }
+function addSkip() {
+    let skipELement = document.createElement("div");
+    skipELement.classList.add(skip);
+    skipELement.textContent = "-";
+    return skipELement;
+}
+const posMain = "item-value-text-positive-main";
+const posSub = "item-value-text-positive-sub";
+const negMain = "item-value-text-negative-main";
+const negSub = "item-value-text-negative-sub";
+const skip = "item-value-text-skip";
 for (const directory of directories) {
     let data = fs.readFileSync(`./templates/${directory}/index.html`, 'utf-8');
     var dom = new JSDOM(data);
-    const posMain = "item-value-text-positive-main";
-    const posSub = "item-value-text-positive-sub";
-    const negMain = "item-value-text-negative-main";
-    const negSub = "item-value-text-negative-sub";
-    const skip = "item-value-text-skip";
     var document = dom.window.document;
     let values = [{
         "current_price": 17500,
@@ -70,25 +75,32 @@ for (const directory of directories) {
         if (values[i].daily_changes > 0) {
             let value = '+' + values[i].daily_changes;
             let valuePerc = '+' + values[i].daily_changes_percent;
-            currentElement = addValue(posMain, value, posSub, valuePerc, currentElement);
-        } else {
-            currentElement = addValue(negMain, values[i].daily_changes, negSub, values[i].daily_changes_percent, currentElement);
+            currentElement.appendChild(addValue(posMain, value, posSub, valuePerc));
+        }
+        if (values[i].daily_changes < 0) {
+            currentElement.appendChild(addValue(negMain, values[i].daily_changes, negSub, values[i].daily_changes_percent));
+        }
+        if (values[i].daily_changes === 0) {
+            currentElement.appendChild(addSkip());
         }
         if (values[i].weekly_changes > 0) {
             let value = '+' + values[i].weekly_changes;
             let valuePerc = '+' + values[i].weekly_changes_percent;
-            currentElement = addValue(posMain, value, posSub, valuePerc, currentElement);
-        } else {
-            currentElement = addValue(negMain, values[i].weekly_changes, negSub, values[i].weekly_changes_percent, currentElement);
+            currentElement.appendChild(addValue(posMain, value, posSub, valuePerc));
+        } if (values[i].weekly_changes < 0) {
+            currentElement.appendChild(addValue(negMain, values[i].weekly_changes, negSub, values[i].weekly_changes_percent));
+        } if (values[i].weekly_changes === 0) {
+            currentElement.appendChild(addSkip());
         }
         if (values[i].monthly_changes > 0) {
             let value = '+' + values[i].monthly_changes;
             let valuePerc = '+' + values[i].monthly_changes_percent;
-            currentElement = addValue(posMain, value, posSub, valuePerc, currentElement);
+            currentElement.appendChild(addValue(posMain, value, posSub, valuePerc));
 
-        } else {
-            currentElement = addValue(negMain, values[i].monthly_changes, negSub, values[i].monthly_changes_percent, currentElement);
-
+        } if (values[i].monthly_changes < 0) {
+            currentElement.appendChild(addValue(negMain, values[i].monthly_changes, negSub, values[i].monthly_changes_percent));
+        } if (values[i].monthly_changes === 0) {
+            currentElement.appendChild(addSkip());
         }
     }
     let currentDate = new Date().toLocaleDateString('ru-RU');
@@ -96,9 +108,9 @@ for (const directory of directories) {
     fs.writeFileSync(`./templates/${directory}/temp.html`, document.documentElement.innerHTML);
 
     const puppeteer = require('puppeteer');
-    puppeteer.launch({headless:'new'}).then(async (browser) => {
+    puppeteer.launch({ headless: 'new' }).then(async (browser) => {
         const page = await browser.newPage();
-        await page.setViewport({width:1140,height:1140})
+        await page.setViewport({ width: 1140, height: 1140 })
         await page.goto(`file://${process.env.TEMP_PATH}/${directory}/temp.html`);
         await page.screenshot({ path: `./reports/${directory}_${currentDate}.png` });
         await browser.close();
