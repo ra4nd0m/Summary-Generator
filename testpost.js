d40a66966986c57ab3b4146e2e080b0dc68ac1c6
Egor Kosyakov <ra4nd0m@gmail.com>
2023-11-02 21:42:26 +0300

    Added file for test post

---
+++ testpost.js
@@ -0,0 +1,44 @@
+const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+const makeSummary = require('./app');
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+async function sendImages(chatId, mediaFiles) {
+    console.log(mediaFiles);
+    const media = mediaFiles.map((file) => ({
+        type: 'photo',
+        media: file,
+    }));
+    try {
+        const result = bot.sendMediaGroup(chatId, media);
+        console.log('Media sent!', result);
+    } catch (err) { console.error('Error sending the media!', err); }
+}
+async function doStuff() {
+    const currentDate = new Date().toLocaleDateString('ru-RU');
+    const directoryPath = './reports';
+    const chatId = '-1002069828441';
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            console.error('Error reading the dir: ', err);
+            return;
+        }
+
+        const filteredFiles = files.filter((file) => file.includes(currentDate));
+        console.log(filteredFiles);
+        const mediaFiles = filteredFiles.map((file) => path.join(directoryPath, file));
+        sendImages(chatId, mediaFiles);
+    });
+}
+
+(async () => {
+    let result = await makeSummary();
+    if (!result) {
+        console.log('Error generating reports!');
+    } else {
+        doStuff();
+    }
+})();
