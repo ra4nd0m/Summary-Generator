d575ac4d6cef7298931b4f47d2c9bbcdd39d08e1
Egor Kosyakov <ra4nd0m@gmail.com>
2024-02-05 19:25:11 +0300

    Added error checking and input to close

--- testrun.js
+++ testrun.js
@@ -1,5 +1,16 @@
 const makeSummary = require("./app");
 
 (async()=>{
+    try{
     await makeSummary();
+    console.log("Success");
+    }catch(err){
+        console.error("Error! ",err);
+    }finally{
+        console.log("Press Ctrl+C to close the app");
+        process.stdin.resume();
+        process.stdin.on('end', () => {
+            process.exit();
+        });
+    }
 })();
