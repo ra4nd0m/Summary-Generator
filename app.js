4ee1c13fe36d0807365c2412fff9021f1018936f
Egor Kosyakov <ra4nd0m@gmail.com>
2024-02-05 19:04:05 +0300

    Added path specification for win

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ async function makeSummary() {
         fs.writeFileSync(`./templates/${directory}/temp.html`, document.documentElement.innerHTML);
 
         const puppeteer = require('puppeteer');
-        await puppeteer.launch({ executablePath: `${process.cwd()}/chromium/chrome.exe`, headless: 'new' }).then(async (browser) => {
+        await puppeteer.launch({ executablePath: `${process.cwd()}/chromium-win/chrome.exe`, headless: 'new' }).then(async (browser) => {
             const page = await browser.newPage();
             await page.setViewport({ width: 1140, height: 1140 });
             const htmlPath = path.join(process.cwd(), 'templates', directory, 'temp.html');
